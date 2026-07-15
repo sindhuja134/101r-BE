dc96bb7901361ef78d0c140531851eed4c2ae2ee
Sindhuja <chsindhuja639@gmail.com>
2026-07-15 16:01:00 +0530

    loops in java

---
+++ src/com/loop/Example1.java
@@ -0,0 +1,32 @@
+package com.loop;
+
+public class Example1 {
+	public boolean isUgly(int num) {
+		if(num<=0) {
+			return false;
+		}
+		while(num%2==0) {
+			num=num/2;
+		}
+		while(num%3==0) {
+			num=num/3;
+		}
+		
+		while(num%5==0) {
+			num=num/5;
+		}
+
+			
+	return num==1;
+	}
+
+	public static void main(String[] args) {
+		Example1 obj=new Example1();
+	System.out.println(obj.isUgly(10)? "ugly number": "!ugly number");
+			
+		}
+		
+
+	}
+
+
---
+++ src/com/loop/Example3.java
@@ -0,0 +1,24 @@
+package com.loop;
+
+public class Example3 {
+	public boolean isPerfect(int num) {
+		if(num<=0) {
+			return false;
+		}
+		int sum=0;
+		for(int i=1;i<=num/2;i++) {
+			if(num%i==0) {
+				sum=sum+i;
+			}
+		}
+		return sum==num;
+	}
+
+	public static void main(String[] args) {
+		Example3 obj=new Example3();
+		System.out.println(obj.isPerfect(6)? "Perfecct Number": "!Perfect Number");
+		
+
+	}
+
+}
